a94eb5d46bdbe39d334a40f77d9d3011756803b2
Bronson Brown <Brsn90@icloud.com>
2020-05-01 19:57:28 -0600

    5/1/2020

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,11 @@ import Axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api'
 
-
-Axios.get(url).then(function (response) {
-    console.log(response);
-}).catch(function (error) {
-    console.log(error);
-}).finally(function () {
-
-});
+export const fetchData = () => {
+    Axios.get(url).then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+        console.log(error);
+    }).finally(function () {
+    })
+}
