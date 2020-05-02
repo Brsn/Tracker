3d3346bec01680616f869cbad8fdf8901d486243
Bronson Brown <Brsn90@icloud.com>
2020-05-01 19:39:27 -0600

    5/1/2020

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import Cards from './components/Cards/Cards';
-import Chart from './components/Chart/Chart';
-import CountryPicker from './components/CountryPicker/CountryPicker';
-import './App.css';
+import logo, { ReactComponent } from './logo.svg';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Cards />
-      <Chart />
-      <CountryPicker />
-    </div>
-  );
+class App extends React.Component() {
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+
+      </div>
+    );
+  }
 }
 
 export default App;
